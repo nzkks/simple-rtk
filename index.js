9bb607c4fe978bbae8e6f8efa9a601a75e6390cb
nzkks <kkshanthosh@gmail.com>
2024-03-30 12:23:05 +1300

    Use the redux logger to log the details in the browser console

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions;
 
-console.log('Initial state ', store.getState());
-
-const unsubscribe = store.subscribe(() => {
-  console.log('updated state ', store.getState());
-});
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
