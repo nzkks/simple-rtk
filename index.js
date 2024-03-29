4e3183b9b2c5f1ba4b8d1cb74af7089c880fe6af
nzkks <kkshanthosh@gmail.com>
2024-03-30 11:59:09 +1300

    index.js: Use the actions from the iceCream slice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions;
 
 console.log('Initial state ', store.getState());
 
@@ -10,5 +11,9 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(2));
 
 unsubscribe();
